6a82fe189aae39891234c9480bde0cf607e28b42
thanikairaajank@gmail.com <thanikairaajank@gmail.com>
2016-01-21 20:42:19 -0600

    WhatToTest
    Test. Drag and Drop

---
+++ WhatToWork/LoginSuccessful.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SQLite;
+using System.Windows.Resources;
+
+namespace WhatToWork
+{
+    /// <summary>
+    /// Interaction logic for LoginSuccessful.xaml
+    /// </summary>
+    public partial class LoginSuccessful : Window
+    {
+        public LoginSuccessful()
+        {
+            InitializeComponent();
+        }
+        String dbConnectionStringLSPage = @"Data Source=database.db;Version=3;";
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+           SQLiteConnection sqliteCon = new SQLiteConnection(dbConnectionStringLSPage);
+           
+            try
+            {
+                sqliteCon.Open();
+                
+                 int generated_Wrk_ID;
+                Random randomNumber= new Random();
+                generated_Wrk_ID = randomNumber.Next(111111,999999);
+
+               // int generated_Wrk_ID=randomNumber.Next(1000,9999);
+
+                bool radioValue=false;
+                bool radioYesVariable=radio_Yes.IsEnabled;
+               bool radioNoVariable=radio_No.IsEnabled;
+
+                if(radioYesVariable)
+                {
+                radioValue=radioYesVariable;
+                }
+                else
+                {
+                radioValue=radioNoVariable;
+                }
+
+
+               // String queryToInsert="Insert into EmployeeInfo(Work_ID,Work_Name,Level,Immediate_Impact,Deadline) values('Wrk_ID','"+this.txt_Work_Name.Text +"','"+this.txt_Level.Text+"','radioValue','"+this.txt_Deadline.Text+"')";
+                String queryToInsert = "Insert into EmployeeInfo(Work_ID,Work_Name,Level,Immediate_Impact,Deadline,Rank) values("+generated_Wrk_ID+",'Work',"+3+","+"'false'"+",'tomorrow',"+5+")";
+
+                SQLiteCommand createCommand = new SQLiteCommand(queryToInsert, sqliteCon);
+               
+                createCommand.ExecuteNonQuery();
+                MessageBox.Show("Saved");
+             sqliteCon.Close();             
+             
+
+                }
+
+            
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void txt_Level_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+    
+
+
+        }
+    }
+
---
+++ WhatToWork/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+
+using System.Data.SQLite;
+namespace WhatToWork
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string dbconnectionString = @"Data Source=database.db;Version=3;";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            SQLiteConnection sqliteCon = new SQLiteConnection(dbconnectionString);
+            // MessageBox.Show("Ha ha ha ha Clicked");
+            try
+            {
+                sqliteCon.Open();
+                String query = "select * from Login_Credentials where username='" + this.txt_Username.Text + "'and password='" + this.txt_Password.Password + "' ";
+                SQLiteCommand createCommand = new SQLiteCommand(query, sqliteCon);
+
+                createCommand.ExecuteNonQuery();
+                SQLiteDataReader dataReader = createCommand.ExecuteReader();
+
+                int count = 0;
+                while (dataReader.Read())
+                {
+                    count++;
+                }
+
+                if (count == 1)
+                {
+                    MessageBox.Show("Credentials are correct!");
+                    sqliteCon.Close();
+                    this.Hide();
+                    LoginSuccessful loginSuccessful = new LoginSuccessful();
+                    loginSuccessful.ShowDialog();
+                
+                
+                }
+
+                if (count > 1)
+                {
+                    MessageBox.Show("Credentials already exist, try again please!");
+                }
+
+                if (count < 1)
+                {
+                    MessageBox.Show("Credentials are incorrect!");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
